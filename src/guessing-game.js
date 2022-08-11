8525c9108457cdd5b3df46c56f138bd134980039
Anna-Grigencha <grigencha10@gmail.com>
2022-08-11 21:21:21 +0300

    feat: add methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,18 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+this.min = min;
+this.max = max;
     }
 
     guess() {
+        return this.middle = Math.floor((this.min + 1 + this.max)/2);
 
     }
 
     lower() {
+        this.max = this.middle;
+        this.guess();
 
     }
 
     greater() {
+        this.min = this.middle;
+        this.guess();
 
     }
 }
